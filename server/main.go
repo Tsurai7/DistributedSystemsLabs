26ac1ec62fe9c050ea1048d512f4b3c84138cbf7
Nikita Kharashun <tsurai7walkin@gmail.com>
2025-03-16 16:30:24 +0300

    fix server

--- server/main.go
+++ server/main.go
@@ -1,67 +1,32 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net"
 	"server/handlers"
-	"strconv"
 	"time"
 )
 
 const (
-	DefaultTcpPort   = 8081
-	DefaultUdpPort   = 9091
-	KeepAlivePeriod  = 30
+	TcpHostPort     = ":8081"
+	UdpHostPort     = ":9091"
+	KeepAlivePeriod = 30
 )
 
 func main() {
-	// Определение флагов командной строки
-	enableUdp := flag.Bool("udp", false, "Enable UDP server")
-	udpPort := flag.Int("udpport", DefaultUdpPort, "UDP server port")
-
-	// Парсинг флагов командной строки
-	flag.Parse()
-
-	// Получение TCP порта из оставшихся аргументов
-	tcpPort := DefaultTcpPort
-	args := flag.Args()
-	if len(args) > 0 {
-		portArg, err := strconv.Atoi(args[0])
-		if err == nil && portArg > 0 && portArg < 65536 {
-			tcpPort = portArg
-		} else {
-			fmt.Printf("Invalid TCP port specified: %s. Using default: %d\n", args[0], DefaultTcpPort)
-		}
-	} else {
-		fmt.Printf("No TCP port specified. Using default: %d\n", DefaultTcpPort)
-	}
-
-	// Формирование адресов серверов
-	tcpHostPort := fmt.Sprintf(":%d", tcpPort)
-	udpHostPort := fmt.Sprintf(":%d", *udpPort)
-
-	// Создание канала для ошибок
 	errChan := make(chan error, 2)
 
-	// Запуск UDP сервера при наличии флага
-	if *enableUdp {
-		go func() {
-			fmt.Printf("Starting UDP server on port %d...\n", *udpPort)
-			err := startUdpServer(udpHostPort)
-			errChan <- err
-		}()
-	}
-
-	// Запуск TCP сервера
 	go func() {
-		fmt.Printf("Starting TCP server on port %d...\n", tcpPort)
-		err := startTcpServer(tcpHostPort)
+		err := startUdpServer()
 		errChan <- err
 	}()
 
-	// Обработка ошибок
+	go func() {
+		err := startTcpServer()
+		errChan <- err
+	}()
+
 	for err := range errChan {
 		if err != nil {
 			log.Fatalf("Server error: %v", err)
@@ -69,14 +34,14 @@ func main() {
 	}
 }
 
-func startTcpServer(hostPort string) error {
-	ln, err := net.Listen("tcp", hostPort)
+func startTcpServer() error {
+	ln, err := net.Listen("tcp", TcpHostPort)
 	if err != nil {
 		return fmt.Errorf("Failed to listen: %v", err)
 	}
 	defer ln.Close()
 
-	fmt.Printf("TCP server listening on %s\n", hostPort)
+	fmt.Printf("TCP server listening on %s\n", TcpHostPort)
 
 	for {
 		conn, err := ln.Accept()
@@ -102,8 +67,8 @@ func startTcpServer(hostPort string) error {
 	}
 }
 
-func startUdpServer(hostPort string) error {
-	addr, err := net.ResolveUDPAddr("udp", hostPort)
+func startUdpServer() error {
+	addr, err := net.ResolveUDPAddr("udp", UdpHostPort)
 	if err != nil {
 		return fmt.Errorf("Failed to resolve UDP address: %v", err)
 	}
@@ -114,7 +79,7 @@ func startUdpServer(hostPort string) error {
 	}
 	defer conn.Close()
 
-	fmt.Printf("UDP server listening on %s\n", hostPort)
+	fmt.Printf("UDP server listening on %s\n", UdpHostPort)
 
 	handlers.HandleUdpConnections(conn)
 
